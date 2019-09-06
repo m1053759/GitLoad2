9de12159eace2bcc9b1faf8191cb0440d0223884
Sai Dash <M1053759@mindtree.com>
2019-09-06 12:51:07 +0530

    3rdCommit

---
+++ HelloProject/src/com/mindtree/com/HelloProgram.java
@@ -0,0 +1,14 @@
+package com.mindtree.com;
+
+public class HelloProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Every1!");
+		System.out.println("This is Mindtree GitHub.");
+		System.out.println("Welcome 2 Possible!");
+		
+	}
+
+}
